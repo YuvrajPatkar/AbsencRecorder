560fff06f0a220d1cf3f2992a382286f078341d0
YuvrajPatkar <patkar.y@etoncollege.org.uk>
2024-02-02 12:02:35 +0000

    Initial Commit

---
+++ AbsencRecorder/AbsencRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsencRecorderApp.swift
+//  AbsencRecorder
+//
+//  Created by Yuvraj Rahul Patkar on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsencRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
